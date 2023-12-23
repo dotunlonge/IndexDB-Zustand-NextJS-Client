e4e97ed07a772830b3f32df714312534c52e0550
Dotun Longe <oludotunlonge@gmail.com>
2023-12-24 00:14:50 +0100

    fix: search-bar-mobile-styling
    Adjustments to search bar appearance on mobile screens.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600&display=swap"
            rel="stylesheet"
          />
+         <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
       <body>
         <Main />
