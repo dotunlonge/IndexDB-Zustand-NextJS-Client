d64ff1843fc7439717f502ea999ddd7a5af47ec9
Dotun Longe <oludotunlonge@gmail.com>
2023-12-23 23:56:15 +0100

    Update user.ts
    year is parsed to a number using parseInt, ensuring that all the parameters passed to the Date constructor are of the correct type

--- src/utils/helpers/user.ts
+++ src/utils/helpers/user.ts
@@ -38,30 +38,36 @@ export const calculateUserStatistics = (users: User[]) => {
  * @param {string} inputDate - input date
  * @returns {Date} A formatted date value
  */
-export function formatDateString(inputDate: string): string {
-      const parts = inputDate.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}) (AM|PM)/);
+ export function formatDateString(inputDate: string): string {
+     const parts = inputDate.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}) (AM|PM)/);
 
-      if (!parts) {
-        return 'Invalid Date';
-      }
+     if (!parts) {
+         return 'Invalid Date';
+     }
 
-      let [ , year, month, day, hour, minute, , ampm] = parts;
+     let [ , year, month, day, hour, minute, , ampm] = parts;
 
-      // Convert hour to 24-hour format if PM and not 12 PM
-      if (ampm === 'PM' && hour !== '12') {
-        hour = (parseInt(hour, 10) + 12).toString();
-      }
+     // Convert hour to 24-hour format if PM and not 12 PM
+     if (ampm === 'PM' && hour !== '12') {
+         hour = (parseInt(hour, 10) + 12).toString();
+     }
 
-      // Convert hour to 0 if 12 AM
-      if (ampm === 'AM' && hour === '12') {
-        hour = '00';
-      }
+     // Convert hour to 0 if 12 AM
+     if (ampm === 'AM' && hour === '12') {
+         hour = '00';
+     }
 
-      // Create a new date object
-      const date = new Date(year, parseInt(month, 10) - 1, parseInt(day, 10), parseInt(hour, 10), parseInt(minute, 10));
+     // Create a new date object with all components parsed as numbers
+     const date = new Date(
+         parseInt(year, 10),
+         parseInt(month, 10) - 1, // Month is 0-indexed
+         parseInt(day, 10),
+         parseInt(hour, 10),
+         parseInt(minute, 10)
+     );
 
      // Format the date
-     const options = {
+     const options: Intl.DateTimeFormatOptions = {
          year: 'numeric',
          month: 'short',
          day: 'numeric',
@@ -71,4 +77,4 @@ export function formatDateString(inputDate: string): string {
      };
 
      return new Intl.DateTimeFormat('en-US', options).format(date);
-};
+ }
