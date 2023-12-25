fb6e34adbcf7995ae0358fbf71d116366d00e330
Dotun Longe <oludotunlonge@gmail.com>
2023-12-25 13:28:19 +0100

    Update HamburgerMenu.tsx

--- src/components/Dashboard/Header/HamburgerMenu.tsx
+++ src/components/Dashboard/Header/HamburgerMenu.tsx
@@ -1,4 +1,4 @@
-// components/HamburgerMenu.tsx
+// components/Dashboard/Header/HamburgerMenu.tsx
 import React from 'react';
 import Image from 'next/image';
 import styles from '@/styles/components/Header.module.scss';
