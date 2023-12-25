6713193257fa68a7e9502355cfb6bae2bc32fead
Dotun Longe <oludotunlonge@gmail.com>
2023-12-25 13:29:04 +0100

    Update index.tsx

--- src/components/Dashboard/Sidebar/index.tsx
+++ src/components/Dashboard/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-// components/Sidebar.tsx
+// components/Dashboard/Sidebar/index.tsx
 import React, { useState, useMemo, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
